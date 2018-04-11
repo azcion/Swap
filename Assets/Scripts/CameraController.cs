462afee0e004531cbcbacb4a6998d2b3873064a6
Azcion <Azcion@users.noreply.github.com>
2018-04-11 20:24:58 +0200

    Set resolution on start

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,20 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Assets.Scripts {
 
 	public class CameraController : MonoBehaviour {
 
 		[UsedImplicitly]
-		private void Start () {
+		private IEnumerator Start () {
+			Screen.SetResolution(419, 543, false);
+
+			while (!SplashScreen.isFinished) {
+				yield return null;
+			}
+
 			Camera.main.transform.position = Z.VCamera;
 		}
 
