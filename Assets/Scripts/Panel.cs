bda4f0b4c255066f2ce12208ceb3e23a8745f77e
Azcion <Azcion@users.noreply.github.com>
2018-04-17 22:42:50 +0200

    Add panel class

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,25 @@
+﻿namespace Assets.Scripts {
+
+	public class Panel {
+
+		public readonly Element Type;
+		public readonly int Health;
+
+		public int Value { get; private set; }
+
+		public Panel (Element type, int health) {
+			Type = type;
+			Health = health;
+		}
+
+		public void AddValue (int value) {
+			Value += value;
+		}
+
+		public void ResetValue () {
+			Value = 0;
+		}
+
+	}
+
+}
--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -6,43 +6,29 @@ namespace Assets.Scripts {
 	public class PanelController : MonoBehaviour {
 
 		[UsedImplicitly]
-		public Sprite[] PanelSprites;
+		public Sprite[] PanelSprites = new Sprite[5];
 
-		private static SpriteRenderer _panel0;
-		private static SpriteRenderer _panel1;
-		private static SpriteRenderer _panel2;
-		private static SpriteRenderer _panel3;
-		private static SpriteRenderer _panel4;
+		private static SpriteRenderer[] _panelRenderers;
 
 		public SpriteRenderer GetPanel (int index) {
-			switch (index) {
-				case 0: return _panel0;
-				case 1: return _panel1;
-				case 2: return _panel2;
-				case 3: return _panel3;
-				case 4: return _panel4;
-				default: return null;
-			}
+			return _panelRenderers[index];
 		}
 
 		private void Debug () {
-			_panel0.sprite = PanelSprites[0];
-			_panel1.sprite = PanelSprites[1];
-			_panel2.sprite = PanelSprites[2];
-			_panel3.sprite = PanelSprites[3];
-			_panel4.sprite = PanelSprites[4];
+			for (int i = 0; i < 5; ++i) {
+				_panelRenderers[i].sprite = PanelSprites[i];
+			}
 		}
 
 		[UsedImplicitly]
 		private void OnEnable () {
 			Vector3 v = transform.localPosition;
 			transform.localPosition = new Vector3(v.x, v.y, Z.Panel);
+			_panelRenderers = new SpriteRenderer[PanelSprites.Length];
 
-			_panel0 = transform.GetChild(0).GetComponent<SpriteRenderer>();
-			_panel1 = transform.GetChild(1).GetComponent<SpriteRenderer>();
-			_panel2 = transform.GetChild(2).GetComponent<SpriteRenderer>();
-			_panel3 = transform.GetChild(3).GetComponent<SpriteRenderer>();
-			_panel4 = transform.GetChild(4).GetComponent<SpriteRenderer>();
+			for (int i = 0; i < _panelRenderers.Length; ++i) {
+				_panelRenderers[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+			}
 		}
 
 		[UsedImplicitly]
